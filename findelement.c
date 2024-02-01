71171f8109f77247f04c3db1a0b3e2f7190dd18a
Deadicated03 <kostasgun@gmail.com>
2024-02-01 16:03:36 +0200

    changes

--- findelement.c
+++ findelement.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <stdio.h> 
 
 
@@ -38,4 +39,46 @@ int main(){
 	printf("position of element in array: %d" , position);
 	
 	
+=======
+#include <stdio.h> 
+
+
+
+int *search(int array[],int size,int number){
+	
+	int *pointer;
+	
+	for(int i = 0; i <= size ; i++){
+		if (array[i] == number){
+			pointer = &array[i];
+			return pointer;
+		}
+	}
+	return NULL;
+	
+}
+
+int main(){
+	
+	int array[] = {1,24,3,34,5,6,73,86,95,10};
+	int size = 10;
+	int number = 5;
+	int *address;
+
+	address = search(array,size,number);
+
+	if(address == NULL){
+		
+		printf("DOES NOT EXIST");
+		
+	}
+	
+	int element = *address;
+	int position = address - array;
+	
+	printf("element in array : %d\n" , element);
+	printf("position of element in array: %d" , position);
+	
+	
+>>>>>>> 971e7b2 (made some changes)
 }
